8f3cba19ee8e50c98bf974a91c5a4e7d3456e2cc
Mateusz Perczak <40388335+losek1@users.noreply.github.com>
2022-06-20 20:20:09 +0200

    Fixed text component

--- src/Components/Settings/Pages/About.jsx
+++ src/Components/Settings/Pages/About.jsx
@@ -7,7 +7,8 @@ const StyledAbout = styled.div`
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  padding: 5px 0 0 10px;
+  gap: 5px;
+  padding: 5px 0 5px 10px;
   border-radius: 6px;
   background: ${({ theme: { taskBar } }) => {
     return taskBar;
@@ -17,17 +18,47 @@ const StyledAbout = styled.div`
 const StyledSpace = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 10px;
+`;
+
+const StyledLink = styled.a`
+  display: flex;
+  color: ${({ theme: { color } }) => {
+    return color;
+  }};
 `;
 
 const AboutApp = () => {
   return (
     <StyledAbout>
       <StyledSpace>
-        <Icon>&#xE7F9;</Icon>
+        <Icon>&#xF5A3;</Icon>
         <SettingText>Luix - 0.0.1</SettingText>
       </StyledSpace>
-      <SettingText>Author: Mateusz Perczak</SettingText>
+      <StyledSpace>
+        <Icon>&#xEB95;</Icon>
+        <SettingText>MIT</SettingText>
+      </StyledSpace>
+      <StyledSpace>
+        <Icon>&#xE167;</Icon>
+        <StyledLink href="https://github.com/MateuszPerczak" target="_blank">
+          <SettingText>Github</SettingText>
+        </StyledLink>
+        <StyledLink href="https://mateuszperczak.github.io/" target="_blank">
+          <SettingText>Website</SettingText>
+        </StyledLink>
+        <StyledLink
+          href="https://github.com/MateuszPerczak/Luix"
+          target="_blank"
+        >
+          <SettingText>Project</SettingText>
+        </StyledLink>
+      </StyledSpace>
+      <StyledSpace>
+        <Icon>&#xE77B;</Icon>
+        <SettingText>Author: Mateusz Perczak</SettingText>
+      </StyledSpace>
     </StyledAbout>
   );
 };
--- src/Components/Settings/SettingText.jsx
+++ src/Components/Settings/SettingText.jsx
@@ -2,6 +2,8 @@ import styled from "@emotion/styled";
 
 const SettingText = styled.span`
   font-size: 20px;
+  padding-top: 5px;
+  line-height: 1;
 `;
 
 export default SettingText;
